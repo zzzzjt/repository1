1b4ce31b75606b3a260cfccb0fcadab7489b81e2
zjt <1571510352@qq.com>
2020-07-25 17:21:33 +0800

    封装请求

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    vue-project1
+    <h1>用户登录</h1>
+    用户名：<input type="text"><br/>
+    密码：<input type="password"><br/>
+    <el-button type="primary" @click="getHandle">发送get请求</el-button>
+    <el-button type="success" @click="postHandle">发送post请求</el-button>
   </div>
 </template>
 <script>
+import { get,post }  from '../request/request';
 export default {
   created() {},
-  methods: {}
+  methods: {
+    getHandle(){
+      get("/users",{
+        params:{
+          page:3,
+          per:2
+        }
+      })
+      .then(res=>console.log(res));
+    },
+    postHandle(){
+      post('/posts',{name:'hali'})
+      .then(
+        res=>console.log(res)
+      );
+    }
+  }
 };
 </script>
 
